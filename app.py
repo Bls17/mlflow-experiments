dc0acfc0ec874e6f36e642f09647f77c610613e4
Badara Diallo <badaradiallo@Host-004.lan>
2025-05-26 21:05:42 +0200

    Link with Dagshub

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ from mlflow.models import infer_signature
 import mlflow.sklearn
 
 import logging
+import dagshub
+
+dagshub.init(repo_owner="Bls17", repo_name="mlflow-experiments", mlflow=True)
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -79,7 +82,7 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
-        remote_server_uri = "https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
+        remote_server_uri = "https://dagshub.com/Bls17/mlflow-experiments.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
